5f14aa27d2e6415fe998106fa53a18b49dfb14b3
Unknown <davidzwa@gmail.com>
2021-08-09 15:18:08 +0200

    UART still works, debug shows working firmware

--- src/boards/NucleoF446/cmsis/stm32f4xx_hal_conf.h
+++ src/boards/NucleoF446/cmsis/stm32f4xx_hal_conf.h
@@ -51,7 +51,8 @@
 /* #define HAL_SDRAM_MODULE_ENABLED    */
 /* #define HAL_HASH_MODULE_ENABLED     */  
 #define HAL_GPIO_MODULE_ENABLED
-/* #define HAL_I2C_MODULE_ENABLED     */
+#define HAL_I2C_MODULE_ENABLED
+#define HAL_FMPI2C_MODULE_ENABLED
 /* #define HAL_I2S_MODULE_ENABLED      */
 /* #define HAL_IWDG_MODULE_ENABLED     */ 
 /* #define HAL_LTDC_MODULE_ENABLED     */
@@ -72,7 +73,7 @@
 #define HAL_CORTEX_MODULE_ENABLED   
 /* #define HAL_PCD_MODULE_ENABLED      */
 /* #define HAL_HCD_MODULE_ENABLED      */
-#define HAL_FMPI2C_MODULE_ENABLED
+// #define HAL_FMPI2C_MODULE_ENABLED
 /* #define HAL_SPDIFRX_MODULE_ENABLED */
 
 
