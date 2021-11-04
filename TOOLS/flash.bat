9b1b1a56a4a945dab8894b06080abb4ceb6a0d8a
Unknown <davidzwa@gmail.com>
2021-11-04 10:54:13 +0100

    Remove WL55 related submodule

---
+++ TOOLS/flash.bat
@@ -0,0 +1,5 @@
+@echo off
+
+echo f | xcopy /s /f  "..\\build\\src\\apps\\ping-pong\\ping-pong.bin" "E:\\ping-pong.bin"
+echo f | xcopy /s /f  "..\\build\\src\\apps\\ping-pong\\ping-pong.bin" "F:\\ping-pong.bin"
+echo f | xcopy /s /f  "..\\build\\src\\apps\\ping-pong\\ping-pong.bin" "G:\\ping-pong.bin"
--- flash.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo f | xcopy /s /f  ".\\build\\src\\apps\\ping-pong\\ping-pong.bin" "E:\\ping-pong.bin"
-echo f | xcopy /s /f  ".\\build\\src\\apps\\ping-pong\\ping-pong.bin" "F:\\ping-pong.bin"
-echo f | xcopy /s /f  ".\\build\\src\\apps\\ping-pong\\ping-pong.bin" "G:\\ping-pong.bin"
