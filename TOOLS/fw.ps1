4f9e1eaf674db55b31cae8dc79c69cc9757c9b3d
David Zwart <davidzwa@gmail.com>
2021-11-04 10:55:23 +0100

    Chore/remove wl55 related (#21)
    * Remove WL55 related submodule
    
    * Remove submodule from .git

---
+++ TOOLS/fw.ps1
@@ -0,0 +1,5 @@
+$src = "..\\build\\src\\apps\\ping-pong\\ping-pong.bin"
+
+$dst = "E:\\ping-pong.bin"
+
+Copy-Item $src -Destination $dst
--- fw.ps1
+++
@@ -1,5 +0,0 @@
-$src = ".\\build\\src\\apps\\ping-pong\\ping-pong.bin"
-
-$dst = "E:\\ping-pong.bin"
-
-Copy-Item $src -Destination $dst
