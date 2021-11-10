e8cf782cd6388a34e71b8865c0a0e764e181284a
Unknown <davidzwa@gmail.com>
2021-11-10 16:29:58 +0100

    Add tasks with radio phy

--- src/apps/ping-pong/NucleoF446/tasks.h
+++ src/apps/ping-pong/NucleoF446/tasks.h
@@ -1,21 +1,10 @@
-#include "timer.h"
 #include "cli.h"
+#include "device_messages.h"
 
 #ifndef TIMERS__H__
 #define TIMERS__H__
 
-static TimerEvent_t BeaconTimer;
-uint16_t BeaconSequence;
-static void OnBeaconEvent(void* context) {
-    BeaconSequence++;
-    printf("Beacon sent %d\n", BeaconSequence);
-    TimerReset(&BeaconTimer);
-}
-
-void InitTimedTasks() {
-    TimerInit(&BeaconTimer, OnBeaconEvent);
-    TimerSetValue(&BeaconTimer, 128000);
-    TimerStart(&BeaconTimer);
-}
+void SetSequenceRequestConfig(const SequenceRequestConfig& config);
+void InitTimedTasks();
 
 #endif  // TIMERS__H__
