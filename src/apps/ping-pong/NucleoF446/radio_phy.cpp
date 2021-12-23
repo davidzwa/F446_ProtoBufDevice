2cf36f68e746099aa33540d90551e90555e635ca
David Zwart <davidzwa@gmail.com>
2021-12-24 00:01:55 +0100

    Also disable sleep on other listener functions

--- src/apps/ping-pong/NucleoF446/radio_phy.cpp
+++ src/apps/ping-pong/NucleoF446/radio_phy.cpp
@@ -134,11 +134,11 @@ void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
 }
 
 void OnRxTimeout(void) {
-    Radio.Sleep();
+    Radio.Rx(0);
 }
 
 void OnRxError(void) {
-    Radio.Sleep();
+    Radio.Rx(0);
 }
 
 void TransmitProtoBuffer() {
