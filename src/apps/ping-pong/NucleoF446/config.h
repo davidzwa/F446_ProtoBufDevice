8371fddad7f938c6127bcc61ba7745cbc1e05826
Unknown <davidzwa@gmail.com>
2022-02-04 10:09:16 +0100

    Adjust SF back to 7 default

--- src/apps/ping-pong/NucleoF446/config.h
+++ src/apps/ping-pong/NucleoF446/config.h
@@ -58,7 +58,7 @@
                                     //  1: 250 kHz, \
                                     //  2: 500 kHz, \
                                     //  3: Reserved]
-#define LORA_SPREADING_FACTOR 11 // [SF7..SF12]
+#define LORA_SPREADING_FACTOR 7 // [SF7..SF12]
 #define LORA_CODINGRATE 2       // [1: 4/5, \
                                     //  2: 4/6, \
                                     //  3: 4/7, \
