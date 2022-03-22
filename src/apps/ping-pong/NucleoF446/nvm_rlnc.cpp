17dc1b3a34103ceb9cecbc55bfa574b268fb9f6f
Unknown <davidzwa@gmail.com>
2022-03-22 10:05:57 +0100

    Design flash layout

--- src/apps/ping-pong/NucleoF446/nvm_rlnc.cpp
+++ src/apps/ping-pong/NucleoF446/nvm_rlnc.cpp
@@ -10,13 +10,28 @@ NvmHandle NvmRlnc(NVM_PAGE);
 // 0x00 is sector header and should only be written to if sector is full
 #define SECTOR_HEADER           ((uint16_t)0x0000)
 
-// Fields useful for later
+// LAYOUT
+// -- Start of header --
+// --> Page header (32 bits)
+// --> Init Size Header (32 bits)
+// --> Termination Size Header (32 bits)
+// -- End of header --
+// --> Packet Rlnc Init (fixed size)
+// --> Packet Rlnc Termination (fixed size)
+// for each g in generations
+//      -- SYNC HEADER 0xFFFF + Length (16+16 bits) --
+//      for all packets in generation
+//          --> RAW meta + fragment
+//      -- SYNC HEADER 0xFFFF + Update Length (16+16 bits) --
+//      --> Packet Rlnc Update (unless g index is 0)
+
+// The serialized Rlnc Init Configuration (Proto, bytes)
 #define ADDR_INIT_SIZE          ((uint16_t)0x0001)
-// Size of packets stored for each generation begin
+// Size of serialized packets stored for each generation begin (bytes)
 #define ADDR_UPDATE_SIZE        ((uint16_t)0x0002)
-// Size of packets stored for each generation's fragments
+// Size of packets stored for each generation's raw fragments
 #define ADDR_FRAG_SIZE          ((uint16_t)0x0003)
-// Size of packet stored for termination
+// Size of packet stored for termination (Proto, bytes)
 #define ADDR_TERM_SIZE          ((uint16_t)0x0004)
 
 #define DATA_SECTOR_BASE        ((uint16_t)0x0004)  // in 32-bits
