Fix data PER estimator with averaging window

tools/stlink_flash_convert.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
 print(os.getcwd())
@@ -7,15 +6,36 @@ print(os.getcwd())
 # 128 64 32 16
 # read_size = 1024 * 16
 # --serial 066BFF323338424E43042719
-subprocess.run(
-    f".\\stlink\\stlink-1.7.0-x86_64-w64-mingw32\\bin\\st-flash.exe read ..\\data\\nucleo 0x08040000 131072", shell=True, check=True)
+# subprocess.run(
+#     f".\\stlink\\stlink-1.7.0-x86_64-w64-mingw32\\bin\\st-flash.exe read ..\\data\\nucleo 0x08040000 131072", shell=True, check=True)
 
 rlnc_sequence_numbers = True
 nullword = ['0xFF', '0xFF', '0xFF', '0xFF']
 nullword_bytes = bytes([int(x, 0) for x in nullword])
 PER_datasets = []
-alpha = 0.2
-marker_size = 3
+alpha = 0.15
+marker_size = 1
+
+### https://stackoverflow.com/questions/61147532/mean-filter-in-python-without-loops
+def meanfilt(x, k):
+    """Apply a length-k mean filter to a 1D array x.
+    Boundaries are extended by repeating endpoints.
+    """
+
+    assert k % 2 == 1, "Mean filter length must be odd."
+    assert x.ndim == 1, "Input must be one-dimensional."
+
+    k2 = (k - 1) // 2
+    y = np.zeros((len(x), k), dtype=x.dtype)
+    y[:, k2] = x
+    for i in range(k2):
+        j = k2 - i
+        y[j:, i] = x[:-j]
+        y[:j, i] = x[0]
+        y[:-j, -(i+1)] = x[j:]
+        y[-j:, -(i+1)] = x[-1]
+    return np.mean(y, axis=1)
+
 
 def signed8(value):
     return -(value & 0x80) | (value & 0x7f)
@@ -30,8 +50,9 @@ def index_to_time(index, rate):
 
 
 def plot_file(path, title, rate):
-    # path = "../data/nucleo"
     file = open(path, "rb")
+
+    # Skip standard headers
     header = file.read(4)
     reserved1 = file.read(4)
     reserved2 = file.read(4)
@@ -47,8 +68,9 @@ def plot_file(path, title, rate):
     data_set_rrsi = []
     data_set_snr = []
     resets = 0
-    while word:
 
+    erasure_tracker = []
+    while word:
         if equals_nullword(word):
             print("Empty word found. Done.")
             break
@@ -58,20 +80,32 @@ def plot_file(path, title, rate):
         rssi = word[1]-150
         snr = signed8(word[0])
 
+        # Startup
         if last_seq_number is None:
             last_seq_number = sequence_number
-        
+
         seq_diff = sequence_number - last_seq_number
         if sequence_number == 0:
             last_seq_number = 0
             resets += 1
-        else:
-            if (seq_diff != 1):
-                print(sequence_number, seq_diff)
+            counter = 0
+            print("Diff 0 (start?)")
+        # else:
+        #     if (seq_diff != 1):
+        #         # Diff is >1, so a packet was missed
+        #         print("Diff != 1", sequence_number, seq_diff - 1)
 
         if seq_diff > 1:
-            print("Packet count missing", seq_diff-1)
-            cum_seq_missed += seq_diff - 1
+            missed_pkts = seq_diff - 1
+            cum_seq_missed += missed_pkts
+
+            miss_entries = [1] * seq_diff
+            miss_entries[-1] = 0
+            erasure_tracker += miss_entries
+
+            print("Packet count missing", missed_pkts, miss_entries)
+        else:
+            erasure_tracker.append(0)
 
         data_set_rrsi.append(rssi)
         data_set_snr.append(snr)
@@ -86,22 +120,34 @@ def plot_file(path, title, rate):
         last_seq_number = sequence_number
         counter += 1
 
+    print("First seq", sequence_number_set[0])
+    print("Last seq", sequence_number_set[-1])
+    print(len(erasure_tracker), "erasure/reception entries")
     print(f"{counter} measurements found, total packets missed {cum_seq_missed}, resets {resets}")
+
+    PER_filter_size = 21
+    PER_output = meanfilt(np.array(erasure_tracker), PER_filter_size)
+    print(data_indices_time[-1])
+    step = 1/rate/3600
+    erasure_indices = np.arange(0, data_indices_time[-1] + step, step)
+    # erasure_indices = range(0, len(PER_output))
+    print("PER mean length", len(erasure_indices), len(PER_output))
 
     fig, axs = plt.subplots(2)
     ax1 = axs[0]
     ax2 = ax1.twinx()
 
     ax3 = axs[1]
-    l3 = ax3.scatter(data_indices_time, PER_set,
-                     alpha=alpha,
-                     s=marker_size,
+    l3 = ax3.scatter(erasure_indices, PER_output,
+                     s=2,
                      color='orange', label='PER')
+    ax3.set_ylim([0, 1])
+    ax3.text(1.5, 0.1, "PER")
     ax4 = ax3.twinx()
     l4 = ax4.scatter(data_indices_time, sequence_number_set,
-                     alpha=alpha,
                      s=marker_size,
                      label='sequence_numbers')
+    ax4.text(3, 4000, "Seq")
     # ax3.legend(handles=l3+l4)
     ax3.set_title("PER over time")
     ax3.set_xlabel('Time (h)')
@@ -111,10 +157,12 @@ def plot_file(path, title, rate):
                      alpha=alpha,
                      s=marker_size,
                      label='RSSI')
+    ax1.text(1, -95, "RSSI")
     l2 = ax2.scatter(data_indices_time, data_set_snr,
                      alpha=alpha,
                      s=marker_size,
                      label='SNR')
+    ax2.text(2, -7, "SNR")
     # ax1.legend(handles=l1+l2)
     ax1.set_xlabel('Time (h)')
     ax1.set_title(title)
@@ -122,20 +170,21 @@ def plot_file(path, title, rate):
     ax2.set_ylabel("SNR [dB]")
 
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
+
     plt.show(block=False)
 
     return PER_set
 
 
 if __name__ == '__main__':
-    base1 = "../data/nucleo"
-    # base = "G:/My Drive/Study/Thesis/Mar 2022/Datasets/"
+    # base1 = "../data/nucleo"
+    base = "D:/Projects/f446-lora/data/datasets/"
 
-    path0 = base1
-    title0 = "RSSI and SNR Roaming"
-    rate0 = 0.5
-    PER_set1 = plot_file(path0, title0, rate0)
-    PER_datasets.append(PER_set1)
+    # path0 = base
+    # title0 = "RSSI and SNR Roaming"
+    # rate0 = 0.5
+    # PER_set1 = plot_file(path0, title0, rate0)
+    # PER_datasets.append(PER_set1)
 
     # path1 = base +"3_a2"
     # title1 = "RSSI and SNR Floor 1 East(Window)"
@@ -149,17 +198,17 @@ if __name__ == '__main__':
     # PER_set2 = plot_file(path2, title2, rate2)
     # PER_datasets.append(PER_set2)
 
-    # path3 = base+"3_a4"
-    # title3  = "RSSI and SNR Floor 3 East(Coffee)"
-    # rate3 = 0.25
-    # PER_set3 = plot_file(path3, title3, rate3)
-    # PER_datasets.append(PER_set3)
+    path3 = base+"4_a4"
+    title3 = "RSSI and SNR Floor 3 East (Coffee machine)"
+    rate3 = 0.25
+    PER_set3 = plot_file(path3, title3, rate3)
+    PER_datasets.append(PER_set3)
 
-    # path4 = base+"3_a6"
-    # title4 = "RSSI and SNR Floor 4 East(Coffee)"
-    # rate4 = 0.25
-    # PER_set4 = plot_file(path4, title4, rate4)
-    # PER_datasets.append(PER_set4)
+    path4 = base+"4_a6"
+    title4 = "RSSI and SNR Floor 4 East (Coffee machine)"
+    rate4 = 0.25
+    PER_set4 = plot_file(path4, title4, rate4)
+    PER_datasets.append(PER_set4)
 
     # path5 = base+"3_a5"
     # title5 = "RSSI and SNR Floor 5 Roam"