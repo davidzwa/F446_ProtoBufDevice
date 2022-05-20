26f52adca5fcc12a85a1b6d47cc3c88a311d3799
Unknown <davidzwa@gmail.com>
2022-05-21 00:01:54 +0200

    Improve device success plot

--- tools/rlnc_prob/plot_fuota_binomial.py
+++ tools/rlnc_prob/plot_fuota_binomial.py
@@ -5,7 +5,7 @@ from shared import failure_rate, pmf_binomial_prob
 # We calculate the probability that a firmware update succeeds
 
 # First contextualize the firmware update
-PER = 0.3
+PER = 0.2
 F = 600E3
 G = 20  # Generation frame count
 q = pow(2, 8)
@@ -16,7 +16,7 @@ f = F / (s_f)  # Frame count
 n_g = f / G  # Generation count
 N = (1+delta) * G
 
-device_perc = 0.98
+device_perc = 0.99
 devices = 3000
 
 print(f"Number of frames {f} gens {n_g}")
@@ -24,33 +24,34 @@ print(f"Number of frames {f} gens {n_g}")
 
 decoding_probs = []
 all_gen_probs = []
-perc98_devices_probs = []
+all_devices_probs = []
+part_devices_probs = []
 
 redundancies = range(0, N+1)
 for redundancy in redundancies:
-    p_fail = failure_rate(G, redundancy, PER, q, True)
-    p_success_decode = 1 - p_fail
-
+    p_fail_decode = failure_rate(G, redundancy, PER, q, True)
+    p_success_decode = 1.0 - p_fail_decode
     
     p_all_gen_success = pow(p_success_decode, n_g)
+    p_all_gen_failure = 1.0 - p_all_gen_success
+    print(p_all_gen_failure)
 
-    p_all_gen_failure = 1 - p_all_gen_success
-
-    p_90perc_failure_prob = 0
+    p_98perc_failure_prob = 0
     min_devices = int(ceil(devices * device_perc))
     for m in range(min_devices, devices+1):
-        p_90perc_failure_prob += pmf_binomial_prob(
+        p_98perc_failure_prob += pmf_binomial_prob(
             p_all_gen_failure, m, devices)
 
     p_all_devices_success = pow(p_all_gen_success, devices)
 
-    # if (p_all_devices_success > 0.9):
-    #     print(f"{redundancy} \tSingleFail:\t{p_fail:.2f} \n\tAllGen({n_g:.0f}):\t{p_all_gen_success:.2f} \n\tAllDevices({devices}):\t{p_all_devices_success:.2f} \n\t98%Devices+:\t{p_90perc_failure_prob:.3f}")
-    #     break
+    # # if (p_all_devices_success > 0.9):
+    # #     print(f"{redundancy} \tSingleFail:\t{p_fail:.2f} \n\tAllGen({n_g:.0f}):\t{p_all_gen_success:.2f} \n\tAllDevices({devices}):\t{p_all_devices_success:.2f} \n\t98%Devices+:\t{p_90perc_failure_prob:.3f}")
+    # #     break
 
-    all_gen_probs.append(p_all_gen_success)
-    perc98_devices_probs.append(p_90perc_failure_prob)
     decoding_probs.append(p_success_decode)
+    all_gen_probs.append(p_all_gen_success)
+    all_devices_probs.append(p_all_devices_success)
+    part_devices_probs.append(p_98perc_failure_prob)
 
 alpha = 0.6
 plt.plot(redundancies, decoding_probs,
@@ -58,14 +59,16 @@ plt.plot(redundancies, decoding_probs,
          label='Decoding Prob', alpha=alpha)
 plt.plot(redundancies, all_gen_probs, '--',
          label='All Gen Prob', alpha=alpha)
-plt.plot(redundancies, perc98_devices_probs, '--',
+plt.plot(redundancies, all_devices_probs, '.',
+         label='100% Devices Prob', alpha=alpha)
+plt.plot(redundancies, part_devices_probs, '--',
          label='98% Devices Prob', alpha=alpha)
 
 plt.grid(True)
 plt.legend()
 plt.xlabel('Redundant packet count')
-plt.ylabel('Decoding Failure Probability')
-plt.title("Completion 60kB FUOTA vs Redundancy (n=20, N=80, PER=0.2")
+plt.ylabel('Decoding Probability')
+plt.title(f"60kB vs Redundancy (n=20,N=80,PER={PER})")
 plt.show()
 
 exit(0)
